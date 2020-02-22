1d2b133caa9973f883df0566354fa7ae66451c22
sbx_user1051 <sbx_user1051@169.254.221.45>
2020-02-22 01:26:52 +0000

    Sat Feb 22 01:26:52 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -37,4 +37,54 @@ public class CheckoutSolution {
 		}
 
 	}
+	
+	private static int charCount(String str, char c) {
+		int count = 0;
+		for(char ch: str.toCharArray()) {
+			if(ch == c) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	private static int charValue(String str, char c) {
+		int value = 0;
+		int charsCount = charCount(str, c);
+		if(c == 'A') {
+			if(charsCount > 3) {
+				value = 130;
+				System.out.println("charsCount ="+charsCount);
+				for(int i=2;i<charsCount-1;i++) {
+					System.out.println("value("+i+")"+value);
+					value += 50;
+				}
+				System.out.println("Total A value ="+value);
+			}
+			else if(charsCount == 3) value = 130;
+			else if(charsCount == 2) value = 100;
+			else value = 50;
+		} else if(c == 'B') {
+			if(charsCount > 2) {
+				value = 45;
+				for(int i=1;i<charsCount-1;i++) {
+					value += 30;
+				}
+			}
+			else if(charsCount == 2) value = 45;
+			else value = 30;
+		} else if(c == 'C') {
+			for(int i=0; i<charsCount;i++) {
+				value += 20;
+			}
+		} else {
+			for(int i=0; i<charsCount;i++) {
+				value += 15;
+			}
+		}
+		
+		return value;
+	}
+
 }
+
