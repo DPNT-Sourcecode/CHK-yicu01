5556b8800d4c922322ab7309109534b227cd053d
sbx_user1051 <sbx_user1051@169.254.230.173>
2020-02-23 18:45:58 +0000

    Sun Feb 23 18:45:58 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -22,7 +22,6 @@ public class CheckoutSolution {
 					charValueA -= 150;
 			} else if(charCount(skus, 'A') > 2 && charCount(skus, 'A') < 5) 
 				charValueA -= 20;
-			System.out.println("Final A ="+charValueA);
 
 			if(charCount(skus, 'B') > 1 && charCount(skus, 'B') < 4) 
 				charValueB -= 15;
@@ -35,8 +34,6 @@ public class CheckoutSolution {
 				charValueB -= 15;
 			else if((charCount(skus, 'E') >= 4 && charCount(skus, 'B') == 2) && (charCount(skus, 'E') < 6 && charCount(skus, 'B') == 2)) 
 				charValueB -= 45;
-
-			System.out.println("Final B ="+charValueB);
 
 			return charValueA + charValueB + charValueC + charValueD + charValueE;
 		} else if(skus.equals("")) return 0;
@@ -61,45 +58,10 @@ public class CheckoutSolution {
 			for(int i=0; i<charsCount;i++) {
 				value += 50;
 			}
-			//			if(charsCount > 3) {
-			//				value = 130;
-			//				for(int i=3;i<charsCount;i++) {
-			//					if(charsCount == 5 && i == 4) {
-			//						value = 200;
-			//					} else if(charsCount == 6 && i == 5) {
-			//						value = 250;
-			//					} else if(charsCount == 7 && i == 6) {
-			//						value = 300;
-			//					} else if(charsCount == 8 && i == 7) {
-			//						value = 330;
-			//					} else if(charsCount == 9 && i == 8) {
-			//						value = 380;
-			//					} else if(charsCount == 10 && i == 9) {
-			//						value = 400;
-			//					} else {
-			//						value += 50;
-			//					}
-			//				}
-			//			}
-			//			else if(charsCount == 3) value = 130;
-			//			else if(charsCount == 2) value = 100;
-			//			else if(charsCount == 1) value = 50;
 		} else if(c == 'B') {
 			for(int i=0; i<charsCount;i++) {
 				value += 30;
 			}
-			//			if(charsCount > 2) {
-			//				value = 45;
-			//				for(int i=2;i<charsCount;i++) {
-			//					if(charsCount > 3 && i == 3) {
-			//						value = 90;
-			//					} else {
-			//						value += 30;
-			//					}
-			//				}
-			//			}
-			//			else if(charsCount == 2) value = 45;
-			//			else if(charsCount == 1) value = 30;
 		} else if(c == 'C') {
 			for(int i=0; i<charsCount;i++) {
 				value += 20;
@@ -118,3 +80,4 @@ public class CheckoutSolution {
 	}
 
 }
+
