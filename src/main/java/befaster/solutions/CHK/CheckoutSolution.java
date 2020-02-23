0f2d392d5aa6d456deb22a545f5ae2ed9f5bbe57
sbx_user1051 <sbx_user1051@169.254.156.29>
2020-02-23 23:28:45 +0000

    Sun Feb 23 23:28:45 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -80,10 +80,8 @@ public class CheckoutSolution {
 				charQ -= 10;
 			else if(charCount(skus, 'Q') >= 6 && charCount(skus, 'Q') < 9) 
 				charQ -= 20;
-			if((charCount(skus, 'R') >= 3 && charCount(skus, 'Q') == 2) && (charCount(skus, 'R') < 6 && charCount(skus, 'Q') == 2)) 
+			if((charCount(skus, 'R') >= 3 && charCount(skus, 'Q') >= 1) && (charCount(skus, 'R') < 6 && charCount(skus, 'Q') >= 1)) 
 				charQ -= 30;
-			else if((charCount(skus, 'R') >= 3 && charCount(skus, 'Q') >= 1) && (charCount(skus, 'R') < 6 && charCount(skus, 'Q') >= 1)) 
-				charQ -= 40;
 			else if((charCount(skus, 'R') >= 6 && charCount(skus, 'Q') == 2) && (charCount(skus, 'R') < 9 && charCount(skus, 'Q') == 2)) 
 				charQ -= 60;
 			if(charCount(skus, 'U') > 3 && charCount(skus, 'U') < 8) 
@@ -238,3 +236,4 @@ public class CheckoutSolution {
 	}
 
 }
+
