01f15a71532e0f501647032a40d8650b7bf9ecb4
sbx_user1051 <sbx_user1051@169.254.6.229>
2020-02-23 13:52:57 +0000

    Sun Feb 23 13:52:57 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -9,10 +9,17 @@ public class CheckoutSolution {
 		int charValueD = charValue(skus, 'D');
 		int charValueE = charValue(skus, 'E');
 		if(skus != null && skus.matches("[A-E]+")) {
+			if(charCount(skus, 'A') >= 5) {
+				charValueA -= 50;
+			} else if(charCount(skus, 'A') >= 3) {
+				charValueA -= 20;
+			}
 			if(charCount(skus, 'E') == 2 && charCount(skus, 'B') >= 1 || charCount(skus, 'E') >= 2 && charCount(skus, 'B') == 1) {
 				charValueB -= 30;
 			} else if(charCount(skus, 'E') == 4 && charCount(skus, 'B') >= 2 || charCount(skus, 'E') >= 4 && charCount(skus, 'B') == 2) {
 				charValueB -= 45;
+			} else if(charCount(skus, 'B') == 2) {
+				charValueB = 45;
 			}
 			return charValueA + charValueB + charValueC + charValueD + charValueE;
 		} else if(skus.equals("")) {
@@ -37,42 +44,48 @@ public class CheckoutSolution {
 		int value = 0;
 		int charsCount = charCount(str, c);
 		if(c == 'A') {
-			if(charsCount > 3) {
-				value = 130;
-				for(int i=3;i<charsCount;i++) {
-					if(charsCount == 5 && i == 4) {
-						value = 200;
-					} else if(charsCount == 6 && i == 5) {
-						value = 250;
-					} else if(charsCount == 7 && i == 6) {
-						value = 300;
-					} else if(charsCount == 8 && i == 7) {
-						value = 330;
-					} else if(charsCount == 9 && i == 8) {
-						value = 380;
-					} else if(charsCount == 10 && i == 9) {
-						value = 400;
-					} else {
-						value += 50;
-					}
-				}
+			for(int i=0; i<charsCount;i++) {
+				value += 50;
 			}
-			else if(charsCount == 3) value = 130;
-			else if(charsCount == 2) value = 100;
-			else if(charsCount == 1) value = 50;
+//			if(charsCount > 3) {
+//				value = 130;
+//				for(int i=3;i<charsCount;i++) {
+//					if(charsCount == 5 && i == 4) {
+//						value = 200;
+//					} else if(charsCount == 6 && i == 5) {
+//						value = 250;
+//					} else if(charsCount == 7 && i == 6) {
+//						value = 300;
+//					} else if(charsCount == 8 && i == 7) {
+//						value = 330;
+//					} else if(charsCount == 9 && i == 8) {
+//						value = 380;
+//					} else if(charsCount == 10 && i == 9) {
+//						value = 400;
+//					} else {
+//						value += 50;
+//					}
+//				}
+//			}
+//			else if(charsCount == 3) value = 130;
+//			else if(charsCount == 2) value = 100;
+//			else if(charsCount == 1) value = 50;
 		} else if(c == 'B') {
-			if(charsCount > 2) {
-				value = 45;
-				for(int i=2;i<charsCount;i++) {
-					if(charsCount > 3 && i == 3) {
-						value = 90;
-					} else {
-						value += 30;
-					}
-				}
+			for(int i=0; i<charsCount;i++) {
+				value += 30;
 			}
-			else if(charsCount == 2) value = 45;
-			else if(charsCount == 1) value = 30;
+//			if(charsCount > 2) {
+//				value = 45;
+//				for(int i=2;i<charsCount;i++) {
+//					if(charsCount > 3 && i == 3) {
+//						value = 90;
+//					} else {
+//						value += 30;
+//					}
+//				}
+//			}
+//			else if(charsCount == 2) value = 45;
+//			else if(charsCount == 1) value = 30;
 		} else if(c == 'C') {
 			for(int i=0; i<charsCount;i++) {
 				value += 20;
@@ -91,3 +104,4 @@ public class CheckoutSolution {
 	}
 
 }
+
